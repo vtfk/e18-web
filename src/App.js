1b3bcf81111eabdbf1ccc7896810fda5152a550e
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-03-02 19:40:56 +0100

    Default layout in place

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { PageNotFound } from './pages/PageNotFound'
 
-import './App.css';
-
 function App() {
   return (
     <Router>
--- src/layouts/Default/index.js
+++ src/layouts/Default/index.js
@@ -1,17 +1,89 @@
-import { Icon, SideNav, SideNavItem } from '@vtfk/components'
+import React from 'react'
+
+import { Icon, IconDropdownNavItem, SideNav, SideNavItem } from '@vtfk/components'
+
+import { TopBar } from '../../components/TopBar'
+import { TopNav, TopNavItem } from '../../components/TopNav'
 
 import './styles.scss'
 
+const user = {
+  displayName: 'Vegar Beider',
+  firstName: 'Vegar',
+  lastName: 'Beider'
+}
+
+const navItems = [
+  {
+    path: '/',
+    icon: {
+      name: 'home'
+    },
+    text: 'Hoppsann'
+  },
+  {
+    path: 'https://www.db.no',
+    icon: {
+      name: 'classes'
+    },
+    text: 'Uppsann'
+  }
+]
+
+const menuItems = [
+  {
+    onClick: () => console.log('Should log off', '🤡'),
+    title: 'Logg av 🤡'
+  }
+]
+
 export function DefaultLayout (props) {
   return (
-    <div className='top-content'>
+    <div className='default-layout'>
+
+      {/* Navigation shown on screen sizes greater than 1000px */}
       <SideNav title='E18'>
-        <SideNavItem href='https://www.vg.no' icon={<Icon name='home' size='medium' />} title='Home' active={window.location.pathname === '/'} />
-        <SideNavItem href='https://www.db.no' icon={<Icon name='search' size='medium' />} title='Search' active={window.location.pathname === '/'} />
-        <SideNavItem href='https://www.tb.no' icon={<Icon name='add' size='medium' />} title='Add' active={window.location.pathname === '/'} />
+        {
+          navItems.map((item, index) => {
+            return (
+              <SideNavItem key={index} href={item.path} icon={<Icon name={item.icon.name} size={item.icon.size || 'medium'} />} title={item.text} active={window.location.pathname === item.path} />
+            )
+          })
+        }
       </SideNav>
-      <div className='main-content'>
-        {props.children}
+
+      {/* Navigation shown on screen sizes less than or equal to 1000px */}
+      <TopNav
+        brandName='E18'
+        displayName={user.displayName}
+        firstName={user.firstName}
+        lastName={user.lastName}
+        menuItems={menuItems}>
+          {
+            navItems.map((item, index) => {
+              return (
+                <TopNavItem key={index} href={item.path} icon={<Icon name={item.icon.name} size={item.icon.size || 'medium'} />} title={item.text} active={window.location.pathname === item.path} />
+              )
+            })
+          }
+      </TopNav>
+
+      <div className='container'>
+
+        {/* Menu bar shown on screen sizes greater than 1000px */}
+        <TopBar displayName={user.displayName} firstName={user.firstName} lastName={user.lastName}>
+          {
+            menuItems.map((item, index) => {
+              return (
+                <IconDropdownNavItem key={index} onClick={item.onClick} title={item.title} closeOnClick={true} />
+              )
+            })
+          }
+        </TopBar>
+
+        <div className='main-content'>
+          {props.children}
+        </div>
       </div>
     </div>
   )
