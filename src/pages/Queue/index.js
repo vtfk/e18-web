418b46b64515972d9988def879f4378b0bb6383e
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-03-09 18:12:34 +0100

    Added some queue stuff

--- src/pages/Queue/index.js
+++ src/pages/Queue/index.js
@@ -1,11 +1,29 @@
+import { StatisticsGroup, StatisticsCard } from '@vtfk/components'
+
 import { DefaultLayout } from '../../layouts/Default'
 
 import './styles.scss'
 
 export function Queue () {
+  function handleStatsClick (type) {
+    console.log(type, 'clicked')
+  }
+
   return (
     <DefaultLayout>
-      Queue
+      
+      <div className='stats'>
+        <StatisticsGroup>
+          <StatisticsCard className='card-type' title='Completed' onClick={() => handleStatsClick('Completed')} value={69} />
+          <StatisticsCard className='card-type' title='Failed' onClick={() => handleStatsClick('Failed')} value={42} />
+          <StatisticsCard className='card-type' title='Retired' onClick={() => handleStatsClick('Retired')} value={12} />
+          <StatisticsCard className='card-type' title='Suspended' onClick={() => handleStatsClick('Suspended')} value={7.5} />
+        </StatisticsGroup>
+      </div>
+      <div className='queue'>
+        Her kommer det en masse queue shit
+      </div>
+
     </DefaultLayout>
   )
 }
